6bd9494790b390ef223355fc818d2f1ce0f0ad92
Nicolas Linares <ing.nicolas.linares@gmail.com>
2026-05-04 10:12:33 -0500

    fix: CloudFront debe reenviar header Authorization al API Gateway (origin request policy AllViewerExceptHostHeader)

--- infra/cdk/stacks/frontend_stack.py
+++ infra/cdk/stacks/frontend_stack.py
@@ -338,6 +338,7 @@ class FrontendStack(Stack):
                     ],
                     cached_methods=["GET", "HEAD"],
                     cache_policy_id="4135ea2d-6df8-44a3-9df3-4b5a84be39ad",  # CachingDisabled
+                    origin_request_policy_id="b689b0a8-53d0-40ab-baf2-68738e2966ac",  # AllViewerExceptHostHeader
                     response_headers_policy_id=security_headers.ref,
                 )
             )
