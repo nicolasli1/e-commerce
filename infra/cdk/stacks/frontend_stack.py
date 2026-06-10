93dda053d823be2a1dda31838b6659d9c45f05cf
Nicolas Linares <ing.nicolas.linares@gmail.com>
2026-06-10 18:39:47 -0500

    Fix Wompi redirect origin and CSP

--- infra/cdk/stacks/frontend_stack.py
+++ infra/cdk/stacks/frontend_stack.py
@@ -90,7 +90,7 @@ class FrontendStack(Stack):
                         content_security_policy=(
                             "default-src 'self'; "
                             "img-src 'self' data: https:; "
-                            "script-src 'self' 'unsafe-inline' https://checkout.wompi.co; "
+                            "script-src 'self' 'unsafe-inline' https://checkout.wompi.co https://*.wompi.co; "
                             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
                             "font-src 'self' https://fonts.gstatic.com; "
                             "connect-src 'self' https:; "
